8005a5eae6d5d9b028e7b39258d1420ce998b22b
Terry Liew <tongko@hotmail.com>
2020-02-20 21:13:52 +0800

    Clean up unwanted files.
    qios2.code-workspace is vscode files.

--- kernel/arch/x86_64/load.asm
+++ kernel/arch/x86_64/load.asm
@@ -1,6 +1,6 @@
 ;== QiOS ======================================================================;
 ;                                                                              ;
-;    Copyright (C) 2017 - 2020 Liew Tze Wei                                    ;
+;    Author - 2020 Liew Tze Wei                                                ;
 ;                                                                              ;
 ;    This file is part of the QiOS kernel, and is made available under the     ;
 ;    terms of The Unlicense (That means just do whatever you want with the     ;
